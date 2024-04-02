5fdb4dafe7b0448886b68e28d272b509a42b8dd2
ninad-react <157863834+ninad-react@users.noreply.github.com>
2024-04-02 10:45:28 +0530

    Multiple reducers

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const redux =  require('redux')
 const createStore = redux.createStore
 
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
 function buyCake() {
     return {
@@ -10,17 +11,59 @@ function buyCake() {
     }
 }
 
+function buyIceCream() {
+    return {
+        type: BUY_ICECREAM
+    }
+}
 // (previousState, action) => newState
 
-const initialState = {
+// const initialState = {
+//     numOfCakes: 10,
+//     numOfIceCreams: 20
+// }
+
+const initialCakeState = {
     numOfCakes: 10
 }
 
-const reducer = (state = initialState, action) => {
+const initialIceCreamState = {
+    numOfIceCreams: 20
+}
+
+// const reducer = (state = initialState, action) => {
+//     switch(action.type){
+//         case BUY_CAKE: return {
+//             ...state,
+//             numOfCakes : state.numOfCakes - 1
+//         }
+
+//         case BUY_ICECREAM: return {
+//             ...state,
+//             numOfIceCreams : state.numOfIceCreams - 1
+//         }
+
+//         default: return state
+//     }
+// }
+
+const cakeReducer = (state = initialCakeState, action) => {
     switch(action.type){
         case BUY_CAKE: return {
             ...state,
             numOfCakes : state.numOfCakes - 1
+        }
+
+        default: return state
+    }
+}
+
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+    switch(action.type){
+
+        case BUY_ICECREAM: return {
+            ...state,
+            numOfIceCreams : state.numOfIceCreams - 1
         }
 
         default: return state
@@ -33,4 +76,6 @@ const unsubscribe = store.subscribe(() => console.log('Updated state', store.get
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
 unsubscribe()
